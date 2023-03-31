869d597bb9bed890e2a655b9bc857672a2ff529a
IgnatenkoKate <123632355+IgnatenkoKate@users.noreply.github.com>
2023-03-31 10:23:18 +0300

    Update settings.py

--- 19.7.2 (HW-04)/settings.py
+++ 19.7.2 (HW-04)/settings.py
@@ -1,3 +1,8 @@
 valid_email = 'kate88-23@mail.ru'
 valid_password = 'zYDGG6fkBtN5b'
 
+invalid_email = 'kate8823@mail.ru'
+invalid_password = 'blabla3bla'
+
+valid_email2 = 'kkkind@mail.ru'
+valid_password2 = 'fRaKMdaPGcvX7'
