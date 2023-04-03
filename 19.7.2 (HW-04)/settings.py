ee75a237821981dfccea287f6d4e2b0c091f7aca
IgnatenkoKate <123632355+IgnatenkoKate@users.noreply.github.com>
2023-04-03 09:06:00 +0300

    Add files via upload

--- 19.7.2 (HW-04)/settings.py
+++ 19.7.2 (HW-04)/settings.py
@@ -4,5 +4,7 @@ valid_password = 'zYDGG6fkBtN5b'
 invalid_email = 'kate8823@mail.ru'
 invalid_password = 'blabla3bla'
 
+invalid_auth_key = {'key': 'blabla3bla'}
+
 valid_email2 = 'kkkind@mail.ru'
 valid_password2 = 'fRaKMdaPGcvX7'
